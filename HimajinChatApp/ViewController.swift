c2e02640cc38b4cd327dff5a98f6044fcfa4565e
cdlscdlscdls <r.bucknumber8@gmail.com>
2020-02-02 15:32:41 +0900

    First Commit

---
+++ HimajinChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HimajinChatApp
+//
+//  Created by jotaro.nishime on 2020/02/02.
+//  Copyright © 2020 jotaro.nishime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
